5c71fd5e279830f98dc408bd7d84efd82661b41f
Peter Heisig <peter.heisig@medien-dresden.de>
2014-06-13 12:07:20 +0200

    exception handling method with annotations

--- webservice/src/main/groovy/de/comprot/ControllerExceptionHandler.groovy
+++ webservice/src/main/groovy/de/comprot/ControllerExceptionHandler.groovy
@@ -2,19 +2,19 @@ package de.comprot
 
 import de.comprot.model.EntityFieldError
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.validation.BindException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @SuppressWarnings("GrMethodMayBeStatic")
 @ControllerAdvice class ControllerExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
-    def handle(BindException exception) {
-        new ResponseEntity<List<EntityFieldError>>(exception.fieldErrors.collect {
-                new EntityFieldError(field: it.field, message: it.defaultMessage)
-        }, HttpStatus.BAD_REQUEST)
+    @ResponseBody def handle(BindException exception) {
+        exception.fieldErrors.collect { new EntityFieldError(field: it.field, message: it.defaultMessage) }
     }
 
 }
