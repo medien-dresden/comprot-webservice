fc62be3d39c93ef5297844b9a68d482852162ff4
Peter Heisig <peter.heisig@medien-dresden.de>
2014-06-16 17:17:43 +0200

    suggestions with the new structure

--- webservice/src/main/groovy/de/comprot/facade/v1/controller/SuggestionsController.groovy
+++ webservice/src/main/groovy/de/comprot/facade/v1/controller/SuggestionsController.groovy
@@ -1,22 +1,29 @@
 package de.comprot.facade.v1.controller
 
-import de.comprot.facade.Version
+import de.comprot.core.service.MappingService
 import de.comprot.core.service.SuggestionService
+import de.comprot.facade.Version
+import de.comprot.facade.v1.model.Suggestion
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping(value = "api/suggestions",
         produces = Version.V1, consumes = Version.V1)
 @RestController class SuggestionsController {
 
-    @Autowired SuggestionService service;
+    @Autowired SuggestionService service
 
+    @Autowired MappingService mappingService
+
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.GET)
-    def getSuggestions(@RequestParam(value = "filter", required = true) String query) {
-        service.getSuggestions(query)
+    def get(@RequestParam(value = "filter", required = true) String query) {
+        mappingService.map(service.getSuggestions(query), Suggestion.class)
     }
 
 }
---
+++ webservice/src/main/groovy/de/comprot/facade/v1/model/Suggestion.groovy
@@ -0,0 +1,5 @@
+package de.comprot.facade.v1.model
+
+import de.comprot.core.model.SuggestionEntity
+
+class Suggestion extends SuggestionEntity {}
