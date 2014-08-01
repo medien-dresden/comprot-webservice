0b2e902ef1b5c599e28a744c9d41793b14b00ddb
Peter Heisig <peter.heisig@medien-dresden.de>
2014-08-01 10:37:31 +0200

    aggregate entity details from index

--- webservice/src/main/groovy/de/comprot/facade/v1/assembler/WorkbenchResourceAssembler.groovy
+++ webservice/src/main/groovy/de/comprot/facade/v1/assembler/WorkbenchResourceAssembler.groovy
@@ -1,6 +1,7 @@
 package de.comprot.facade.v1.assembler
 
 import de.comprot.core.model.WorkbenchEntity
+import de.comprot.core.service.ComprotEntityIndexService
 import de.comprot.core.service.MappingService
 import de.comprot.facade.v1.controller.UserController
 import de.comprot.facade.v1.controller.WorkbenchController
@@ -15,12 +16,33 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
 
     @Autowired MappingService mappingService
 
+    @Autowired ComprotEntityIndexService indexService
+
+    @Autowired ComprotEntityResourceAssembler entityResourceAssembler
+
     WorkbenchResourceAssembler() {
         super(WorkbenchController, WorkbenchDto)
     }
 
     @Override WorkbenchDto toResource(WorkbenchEntity workbench) {
         def dto = createResourceWithId workbench.id, workbench
+
+        dto.targets = []
+        dto.compounds = []
+
+        workbench.compounds.each { entity ->
+            def indexEntity = indexService.getEntity entity.type, entity.comprotId
+            def entityResource = entityResourceAssembler.toResource indexEntity
+
+            dto.compounds.add entityResource
+        }
+
+        workbench.targets.each { entity ->
+            def indexEntity = indexService.getEntity entity.type, entity.comprotId
+            def entityResource = entityResourceAssembler.toResource indexEntity
+
+            dto.targets.add entityResource
+        }
 
         dto.add(linkTo(UserController)
                 .slash(workbench.user.username)
